f5a794ecaca7ad3880103bdc658e6428d7e3b223
11cconnolly <callumc11@gmail.com>
2021-10-10 22:21:22 +0100

    Setup base for concurrent browser actions

--- src/Act.js
+++ src/Act.js
@@ -36,10 +36,32 @@ const render = (element, container) => {
       dom[name] = element.props[name];
     });
 
+  // This recursive call won't stop until the complete tree is rendered, getting in the way
+  // Of other potential actions the browser needs to do e.g. handling user input
   element.props.children.forEach((child) => render(child, dom));
 
   container.appendChild(dom);
 };
+
+const performUnitOfWork = (nextUnitOfWork) => {
+  //TODO
+};
+
+let nextUnitOfWork = null;
+
+// The deadline passed in tells us how long we have until the browser needs control again
+const workLoop = (deadline) => {
+  const shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+};
+
+// Performs set actions when the main thread is idle https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback
+// Part of the window API which is the currently running window that's exposed to the JS code that's running
+requestIdleCallback(workLoop);
 
 const Act = {
   createElement,
