92bbb72df4f019401678543b8ad8fb156cd77916
11cconnolly <callumc11@gmail.com>
2021-10-10 20:43:31 +0100

    Working setup for development

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Act from "./Act";
+
+/** @jsx Act.createElement */
+const element = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Didact</h2>
+  </div>
+);
+const container = document.getElementById("root");
+Act.render(element, container);
