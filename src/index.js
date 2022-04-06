3dc462153e906e7448c3f5dfcd124c2d3b19666e
11cconnolly <callumc11@gmail.com>
2022-04-06 21:19:48 +0100

    Minor change to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Act from "./Act";
 
 /** @jsx Act.createElement */
+// The following JSX code gets transpiled by our React clone
 // TODO Make the style objects passed in an object
 const element = (
   <div id="foo" style="background: salmon; width: 200px; height: 300px">
